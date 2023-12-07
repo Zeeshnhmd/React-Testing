4ed5107891c0549589d84fa31ce99d3028e9db59
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-12-08 00:02:22 +0530

    Tutorial 12: Grouping tests

--- src/components/Tutorial9/Greet.test.tsx
+++ src/components/Tutorial9/Greet.test.tsx
@@ -1,4 +1,6 @@
 // TTD: Test Driven Development
+// We can use Describe method to wrap test inside "But it works when you run that file only"
+// We can also use .skip, .only method which comes inside test and descride to skip the test, to run that test only
 
 import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
@@ -6,14 +8,16 @@ import Greet from './Greet';
 // Greet should render the test hello and if a name is passed into the Component
 // It should render hello followed by the name
 
-test('Greet render Correctly', () => {
-	render(<Greet />);
-	const textELement = screen.getByText(/hello/i);
-	expect(textELement).toBeInTheDocument();
-});
+describe('Great', () => {
+	test('render Correctly', () => {
+		render(<Greet />);
+		const textELement = screen.getByText(/hello/i);
+		expect(textELement).toBeInTheDocument();
+	});
 
-test('Greet render with name', () => {
-	render(<Greet name="Zeeshan" />);
-	const textElement = screen.getByText(/hello zeeshan/i);
-	expect(textElement).toBeInTheDocument();
+	test('render with name', () => {
+		render(<Greet name="Zeeshan" />);
+		const textElement = screen.getByText(/hello zeeshan/i);
+		expect(textElement).toBeInTheDocument();
+	});
 });
