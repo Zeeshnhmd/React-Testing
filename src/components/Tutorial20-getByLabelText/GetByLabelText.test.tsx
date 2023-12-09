08b9acd7b42e0c4e03a9a5a4ef3b2c0c86750e32
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-12-09 21:46:23 +0530

    Tutorial 20: GetByLabelText

---
+++ src/components/Tutorial20-getByLabelText/GetByLabelText.test.tsx
@@ -0,0 +1,20 @@
+/**
+ // * GetByLabelText check if label is present or not.
+ // * Suppose if we have multiple element who have same labels text. In this case we can use the "Selector" option to determine which label we want to get tested.
+ */
+
+import { render, screen } from '@testing-library/react';
+import { GetByLabelText } from '.';
+
+test('Label renders Correctly', () => {
+	render(<GetByLabelText />);
+	const labelElement1 = screen.getByLabelText('Bio', {
+		selector: 'textarea',
+	});
+	expect(labelElement1).toBeInTheDocument();
+
+	const labelElement2 = screen.getByLabelText('Bio', {
+		selector: 'input',
+	});
+	expect(labelElement2).toBeInTheDocument();
+});
---
+++ src/components/Tutorial20-getByLabelText/index.tsx
@@ -0,0 +1,14 @@
+export const GetByLabelText = () => {
+	return (
+		<>
+			<div>
+				<label htmlFor="bio">Bio</label>
+				<textarea name="bio" id="bio"></textarea>
+			</div>
+			<div>
+				<label htmlFor="Name">Bio</label>
+				<input name="Name" id="Name"></input>
+			</div>
+		</>
+	);
+};
